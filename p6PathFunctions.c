74f1258530c9c738d712977be413ff3d14f45f38
donovanolivarez <34660331+donovanolivarez@users.noreply.github.com>
2019-05-04 11:40:11 -0500

    Add files via upload

---
+++ p6PathFunctions.c
@@ -0,0 +1,17 @@
+/*
+    new functions for pgm6. deleteAirport will be in the flightdata function.
+ */
+
+void prtAlts(Graph graph, int iOriginVertex, int iDestVertex)
+{
+    // do stuff
+}
+int maxStepsChron(Graph graph, int iVertex, int iDestVertex, int iPrevArrTm2400)
+{
+    // do stuff 
+}
+
+void determinePaths(Graph graph, int iVertex, int iDestVertex, int iCurStep, Path path)
+{
+    //do stuff
+}
